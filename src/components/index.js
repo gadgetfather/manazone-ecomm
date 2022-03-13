13aeefe3224d25940a8ef6c2fcd161fb046516ec
Akshay Kamble <kamblea635@gmail.com>
2022-03-13 18:02:49 +0530

    add exports
    for ProductCard, ProductGrid

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,9 @@
 export { Aside } from "./Aside/Aside";
+
 export { Navbar } from "./Navbar/Navbar";
 export { CartCard } from "./CartCard/CartCard";
 export { CategoriesCard } from "./CategoriesCard/CategoriesCard";
 export { HeroSection } from "./HeroSection/HeroSection";
 export { PromotionalCard } from "./PromotionalCard/PromotionalCard";
+export { ProductCard } from "./ProductCard/ProductCard";
+export { ProductGrid } from "./ProductGrid/ProductGrid";
