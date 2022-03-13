00b348d83e8bdea5dd99741e60e1c5e0b4aa40f4
Akshay Kamble <kamblea635@gmail.com>
2022-03-13 14:26:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import logo from "./logo.png";
 
 function App() {
-  return null;
+  const [productData, setProductData] = useState([]);
+  async function getProducts() {
+    const { data } = await axios.get("/api/categories");
+    console.log(data);
+    setProductData(data.categories);
+  }
+  useEffect(getProducts, []);
+  console.log(productData);
+  return (
+    <>
+      <h1>hello</h1>
+      {productData.map((item) => {
+        return (
+          <>
+            <h1>{item.title}</h1>
+            <img src={item.image} alt="" />
+            <h2>Rs.{item.price}</h2>
+          </>
+        );
+      })}
+    </>
+  );
 }
 
 export default App;
