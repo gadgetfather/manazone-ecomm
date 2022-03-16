807a0f823eb71fed8050010bfb232ea8d2830491
Akshay Kamble <kamblea635@gmail.com>
2022-03-16 13:48:25 +0530

    create filter context

---
+++ src/context/filter-context.js
@@ -0,0 +1,36 @@
+import { createContext, React, useReducer, useContext } from "react";
+
+const FilterContext = createContext();
+
+const initialObj = {
+  sortBy: null,
+  category: null,
+  rating: null,
+};
+
+const FilterProvider = ({ children }) => {
+  const [filter, filterDispatch] = useReducer(filterReducer, initialObj);
+
+  function filterReducer(state, action) {
+    switch (action.type) {
+      case "SORT":
+        return { ...state, sortBy: action.payload };
+      case "RESET":
+        return initialObj;
+      case "RATING":
+        return { ...state, rating: action.payload };
+      default:
+        return state;
+    }
+  }
+
+  return (
+    <FilterContext.Provider value={{ filterDispatch, filter }}>
+      {children}
+    </FilterContext.Provider>
+  );
+};
+
+const useFilter = () => useContext(FilterContext);
+
+export { useFilter, FilterProvider };
