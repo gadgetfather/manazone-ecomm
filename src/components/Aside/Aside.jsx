10e12e52120c141130920a2d9e1b6f134aa87103
Akshay Kamble <kamblea635@gmail.com>
2022-03-16 13:50:21 +0530

    implemented filter functionality
    By stars and by price

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { useFilter } from "../../context/filter-context";
 import "./Aside.css";
+
 export function Aside() {
+  const {
+    filterDispatch,
+    filter: { sortBy, rating },
+  } = useFilter();
+
   return (
     <>
       <aside>
         <span className="filter-title">
           <h2>Filters</h2>
-          <button className="btn btn-text-secondary"> clear</button>
+          <button
+            onClick={() => filterDispatch({ type: "RESET" })}
+            className="btn btn-text-secondary"
+          >
+            {" "}
+            clear
+          </button>
         </span>
         <h2>Price</h2>
         <input type="range" className="slider" />
@@ -30,33 +43,82 @@ export function Aside() {
 
         <h2>Rating</h2>
         <span>
-          <input type="radio" name="star" />
+          <input
+            type="radio"
+            checked={rating && rating === "FOUR_STAR"}
+            onChange={() =>
+              filterDispatch({ type: "RATING", payload: "FOUR_STAR" })
+            }
+            name="star"
+          />
           <label htmlFor="4star">4 Stars & above</label>
         </span>
         <span>
-          <input type="radio" name="star" />
+          <input
+            type="radio"
+            checked={rating && rating === "THREE_STAR"}
+            onChange={() =>
+              filterDispatch({ type: "RATING", payload: "THREE_STAR" })
+            }
+            name="star"
+          />
           <label htmlFor="3star">3 Stars & above</label>
         </span>
         <span>
-          <input type="radio" name="star" />
+          <input
+            type="radio"
+            checked={rating && rating === "TWO_STAR"}
+            onChange={() =>
+              filterDispatch({ type: "RATING", payload: "TWO_STAR" })
+            }
+            name="star"
+          />
           <label htmlFor="2star">2 Stars & above</label>
         </span>
         <span>
-          <input type="radio" name="star" />
+          <input
+            type="radio"
+            checked={rating && rating === "ONE_STAR"}
+            onChange={() =>
+              filterDispatch({ type: "RATING", payload: "ONE_STAR" })
+            }
+            name="star"
+          />
           <label htmlFor="1star">1 Star & above</label>
         </span>
 
         <h2>Sort by</h2>
         <span>
-          <input type="radio" name="sort" />
-          <label htmlFor="HtoL">High to Low</label>
+          <input
+            type="radio"
+            checked={sortBy && sortBy === "HIGH_TO_LOW"}
+            onChange={() =>
+              filterDispatch({ type: "SORT", payload: "HIGH_TO_LOW" })
+            }
+            name="sort"
+          />
+          <label htmlFor="HIGH_TO_LOW">High to Low</label>
         </span>
         <span>
-          <input type="radio" name="sort" />
+          <input
+            type="radio"
+            checked={sortBy && sortBy === "LOW_TO_HIGH"}
+            onChange={() =>
+              filterDispatch({ type: "SORT", payload: "LOW_TO_HIGH" })
+            }
+            name="sort"
+          />
           <label htmlFor="LtoH">Low to High</label>
         </span>
         <span>
-          <input type="radio" name="sort" />
+          <input
+            type="radio"
+            checked={sortBy && sortBy === "NEW_TO_OLD"}
+            onChange={() =>
+              filterDispatch({ type: "SORT", payload: "NEW_TO_OLD" })
+            }
+            name="sort"
+          />
           <label htmlFor="NtoO">New to Old</label>
         </span>
         <span>
@@ -64,6 +126,9 @@ export function Aside() {
           <label htmlFor="OtoN">Old to New</label>
         </span>
       </aside>
+
+      {/* mobile layout */}
+
       <div className="filter-layout-mob">
         <span className="filter-title">
           <h2>Filters</h2>
@@ -111,19 +176,19 @@ export function Aside() {
       <div className="sort-layout-mob">
         <h2>Sort by</h2>
         <span>
-          <input type="radio" name="sort" />
+          <input type="radio" name="sort-mob" />
           <label htmlFor="HtoL">High to Low</label>
         </span>
         <span>
-          <input type="radio" name="sort" />
+          <input type="radio" name="sort-mob" />
           <label htmlFor="LtoH">Low to High</label>
         </span>
         <span>
-          <input type="radio" name="sort" />
+          <input type="radio" name="sort-mob" />
           <label htmlFor="NtoO">New to Old</label>
         </span>
         <span>
-          <input type="radio" name="sort" />
+          <input type="radio" name="sort-mob" />
           <label htmlFor="OtoN">Old to New</label>
         </span>
       </div>
--- src/components/ProductGrid/ProductGrid.jsx
+++ src/components/ProductGrid/ProductGrid.jsx
@@ -1,12 +1,50 @@
 import React, { useState } from "react";
 import "./ProductGrid.css";
 import { ProductCard, Loader } from "../index";
+import { useFilter } from "../../context/filter-context";
 
 export function ProductGrid({ products, loader }) {
+  const {
+    filter: { sortBy, rating },
+  } = useFilter();
+  function getSortedData(data, sortBy) {
+    console.log("at start", sortBy);
+    const newData = [...data];
+    if (sortBy === "HIGH_TO_LOW") {
+      return newData.sort((a, b) => b.price - a.price);
+    }
+    if (sortBy === "LOW_TO_HIGH") {
+      return newData.sort((a, b) => a.price - b.price);
+    }
+
+    return data;
+  }
+  function getFilteredData(sortedData, Rating) {
+    const newData = [...sortedData];
+    if (Rating === "ONE_STAR") {
+      return newData.filter((item) => item.stars >= 1);
+    }
+    if (Rating === "TWO_STAR") {
+      console.table(newData);
+      return newData.filter((item) => item.stars >= 2);
+    }
+    if (Rating === "THREE_STAR") {
+      console.table(newData);
+      return newData.filter((item) => item.stars >= 3);
+    }
+    if (Rating === "FOUR_STAR") {
+      console.table(newData);
+      return newData.filter((item) => item.stars >= 4);
+    }
+    return sortedData;
+  }
+
+  const sortedData = getSortedData(products, sortBy);
+  const filteredData = getFilteredData(sortedData, rating);
   return (
     <div className="product-section">
       {loader && <Loader />}
-      {products.map((item, indx) => (
+      {filteredData.map((item, indx) => (
         <ProductCard key={indx} {...item} />
       ))}
     </div>
