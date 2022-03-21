eac51520153b8305bf08bb4692757e1459bd730e
Akshay Kamble <kamblea635@gmail.com>
2022-03-21 14:01:33 +0530

    update price part

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,28 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CartCard } from "../../components";
+import { useCart } from "../../context/cart-context";
 import "./CartPage.css";
 export function CartPage() {
+  const { cartData } = useCart();
+  const [totalPrice, setTotalPrice] = useState();
+
+  useEffect(
+    () =>
+      setTotalPrice(
+        cartData.reduce((acc, curr) => acc + Number(curr.price * curr.qty), 0)
+      ),
+    [cartData]
+  );
+
   return (
     <main className="main-content_cart">
-      <div className="cart-total card-container card-shadow">
-        <h1 className="">Price-details</h1>
-        <span className="pricing-details">
-          <span className="product-names">
-            <p>Price</p>
-            <p>Discount</p>
-            <p>Delivery Charges</p>
-            <h3 className="breakr-top">Total Amount</h3>
+      {cartData.length == 0 ? (
+        <h1>Cart is empty :( </h1>
+      ) : (
+        <div className="cart-total card-container card-shadow">
+          <h1 className="">Price-details</h1>
+          <span className="pricing-details">
+            <span className="product-names">
+              <p>Price</p>
+
+              <p>Delivery Charges</p>
+              <h3 className="breakr-top">Total Amount</h3>
+            </span>
+            <span className="product-prices">
+              <p>₹{totalPrice}</p>
+
+              <p>₹200</p>
+              <p className="breakr-top">₹{totalPrice + 200}</p>
+            </span>
           </span>
-          <span className="product-prices">
-            <p>₹6000</p>
-            <p>₹1000</p>
-            <p>₹200</p>
-            <p className="breakr-top">₹5200</p>
-          </span>
-        </span>
-        <button className="btn btn-primary">Proceed to buy</button>
-      </div>
-      <CartCard />
+          <button className="btn btn-primary">Proceed to buy</button>
+        </div>
+      )}
+      {cartData.map((item) => (
+        <CartCard key={item.id} {...item} />
+      ))}
     </main>
   );
 }
