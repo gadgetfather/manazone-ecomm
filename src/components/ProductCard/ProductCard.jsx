6b20b0d5959a1897994b3bc494012217aa7f69b3
Akshay Kamble <kamblea635@gmail.com>
2022-03-23 09:12:54 +0530

    add wishlist functionality to productCard

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,19 +1,35 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { removeItemFromWishlistHandler } from "../../backend/controllers/WishlistController";
 import { useCart } from "../../context/cart-context";
+import { useWishlist } from "../../context/wishlist-context";
 
 import "./ProductCard.css";
 export function ProductCard(props) {
   const navigate = useNavigate();
-  const { title, image, price, categoryName, stars, badge, id } = props;
-  const { addToCart, cartData } = useCart();
+
   const token = localStorage.getItem("Manazone.Token");
+
+  const { title, image, price, categoryName, stars, badge, id, _id } = props;
+  const { addToCart, cartData } = useCart();
+  const { addToWishlist, wishlistData, removeFromWishlist } = useWishlist();
+
   const handleAddToCart = (props) => {
     if (token) {
       addToCart(props);
     } else {
       navigate("/login");
     }
+  };
+  const handleAddToWishlist = (data) => {
+    if (Token) {
+      addToWishlist(data);
+    } else {
+      navigate("/login");
+    }
+  };
+  const handleRemoveFromWishlist = (data) => {
+    removeFromWishlist(data);
   };
 
   return (
@@ -23,7 +39,17 @@ export function ProductCard(props) {
         <img className="card-img" src={image} alt="..." />
       </div>
       <span className="fav-icon">
-        <i className="fas fa-heart"></i>
+        {wishlistData.some((item) => item.id === id) ? (
+          <i
+            onClick={() => handleRemoveFromWishlist(_id)}
+            className="fas fa-heart red "
+          ></i>
+        ) : (
+          <i
+            onClick={() => handleAddToWishlist(props)}
+            className="fas fa-heart "
+          ></i>
+        )}
       </span>
       <div>
         <h1 className="card-title">{title}</h1>
