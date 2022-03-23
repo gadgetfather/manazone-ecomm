f7696c4fdb54e46ef062f105a2dfb8290c76b547
Akshay Kamble <kamblea635@gmail.com>
2022-03-23 09:13:50 +0530

    add functionality on wishlist page

--- src/pages/WishlistPage/WishlistPage.jsx
+++ src/pages/WishlistPage/WishlistPage.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { ProductCard } from "../../components";
+import { WishlistCard } from "../../components";
+import { useWishlist } from "../../context/wishlist-context";
 import "./WishlistPage.css";
-import { ProductGrid } from "../../components/index";
 export function WishlistPage() {
+  const { wishlistData } = useWishlist();
   return (
     <main className="main-content">
       <h1 className="page-title">My Wishlist</h1>
-
-      <div className="wishlist-section">
-        <ProductCard />
-      </div>
+      {wishlistData.length === 0 ? (
+        <h1 className="page-subtitle_wishlist">Wishist list empty :(</h1>
+      ) : (
+        <div className="wishlist-section">
+          {wishlistData.map((item) => (
+            <WishlistCard key={item.id} {...item} />
+          ))}
+        </div>
+      )}
     </main>
   );
 }
