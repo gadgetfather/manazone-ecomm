3e9c18c98fca07c7d3a323e5296e779d9996251e
Akshay Kamble <kamblea635@gmail.com>
2022-04-22 10:35:28 +0530

    created profile page

---
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useAuth } from "../../context/auth-context";
+import "./ProfilePage.css";
+export function ProfilePage() {
+  const { authInfo } = useAuth();
+  console.log(authInfo);
+  return (
+    <main className="main-content_error">
+      <div className="form-container">
+        <h1>Profile</h1>
+        <div className="information-field">
+          <span>First name:</span>
+          <span>{authInfo.user.firstName}</span>
+        </div>
+        <div className="information-field">
+          <span>Last name:</span>
+          <span>{authInfo.user.lastName}</span>
+        </div>
+        <div className="information-field">
+          <span>Email:</span>
+          <span>{authInfo.user.email}</span>
+        </div>
+      </div>
+      <Link className="btn btn-primary" to={"/"}>
+        Go back
+      </Link>
+    </main>
+  );
+}
