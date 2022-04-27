62a7e67e037c88c4eb41ba9f5ba2b738041a834c
Akshay Kamble <kamblea635@gmail.com>
2022-04-27 20:37:25 +0530

    create searchbar-mobile component

---
+++ src/components/SearchbarMobile/SearchbarMobile.jsx
@@ -0,0 +1,74 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+export function SearchbarMobile({ searchValue, setSearchValue }) {
+  const [searchResult, setSearchResult] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  const navigate = useNavigate();
+  async function getProducts() {
+    try {
+      const { data } = await axios.get("/api/products");
+
+      setProducts(data.products);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(getProducts, []);
+  const filterProduct = (e, data) => {
+    let inputVal = e.target.value.toLowerCase().trim();
+    let inputLen = inputVal.length;
+    const result = data.filter(
+      (item) =>
+        item.title.toLowerCase().substring(0, inputLen).includes(inputVal) ||
+        item.categoryName
+          .toLowerCase()
+          .substring(0, inputLen)
+          .includes(inputVal)
+    );
+    console.log(result);
+    if (inputLen > 0) {
+      result.length > 0
+        ? setSearchResult(result)
+        : setSearchResult([{ id: 0, title: "not found" }]);
+    } else {
+      setSearchResult([]);
+    }
+  };
+  const navigateToProduct = (id) => {
+    navigate(`/products/${id}`);
+    setSearchValue("");
+  };
+  console.log(searchValue);
+  return (
+    <div className="search-bar-container-mobile">
+      <div className="search-bar-mobile">
+        <i className="fas fa-search"></i>
+        <input
+          value={searchValue}
+          onChange={(e) => {
+            setSearchValue(e.target.value);
+            filterProduct(e, products);
+          }}
+          type="search"
+        />
+      </div>
+      {searchValue.length > 0 ? (
+        <div className="searchbox-mobile">
+          {searchResult.map((item) => (
+            <p
+              className="search-result_item"
+              onClick={() => navigateToProduct(item.data_id)}
+            >
+              {item.title}
+            </p>
+          ))}
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
